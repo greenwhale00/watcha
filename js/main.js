5e77e908db3015753bc55ee735dd813d872e02d5
greenwhale00 <goreakay@gmail.com>
2022-09-01 16:16:13 +0900

    220901

---
+++ js/main.js
@@ -0,0 +1,31 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const MainSlider = new Swiper('.mySwiper', {
+        slidesPerView: 1,
+        loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    })
+
+    const CubeSlider = new Swiper('.iceSwiper', {
+        slidesPerView: 3,
+        spaceBetween: 0,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    })
+
+
+
+})
